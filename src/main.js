21c52b0f6801f52630072e4e6eac00dd7ceae7bd
周智伟 <zhouzhiwei@laihua.com>
2022-09-18 18:28:09 +0800

    完善

--- src/main.js
+++ src/main.js
@@ -1,25 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './test.js'
-
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import './test.js';
 createApp(App).mount('#app');
-
 class AAA {
     test() {
         console.log(this);
     }
 }
-
-class BBB extends AAA {
-    test: any;
+class BBB {
+    test;
 }
-
 let bbb = new BBB();
 let aaa = new AAA();
 bbb.test = aaa.test;
 bbb.test();
-
 debugger;
 let numer = 'VBNDVNEMDVDMVDMVDVDV';
 console.log(typeof numer);
+//# sourceMappingURL=main.js.map
---
+++ src/test.js
@@ -0,0 +1,15 @@
+
+
+class A {
+    a = 0;
+}
+
+class B extends A {
+    b = 2;
+}
+
+let a  = new A();
+let b = new B();
+
+debugger;
+console.log('finish');
